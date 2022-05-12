81d5105a5b66db64292df472e29af1aa9267d4b6
Mamadou <105246027+Mamadou2211@users.noreply.github.com>
2022-05-12 16:54:21 +0100

    Update help.js

--- discord/commands/info/help.js
+++ discord/commands/info/help.js
@@ -6,7 +6,7 @@ module.exports = {
     run: async(client, message, args) => {
       let embed = new MessageEmbed()
       .setTitle(`${client.user.username}'s Help Page`)
-      .setDescription(`This help page shows all the command of this bot.\n\n**Commands**\n\`help\`\nShow the help page.\n\n\`ping\`\nReturns bot ping.\n\n\n**Slash Commands**\n\`ping\`\nReturns bot ping.\n\n\`parent-category\`\nSet the parent category of ticket channels\n\n\`send-channel\`\nSet the channel to send the ticket panel\n\n\`transcript-set\`\nSet the channel to send transcript & the type of transcrip\n\n\`send-panel\`\nSend the ticket panel.\n\n\`create\`\nCreate a new the ticket\n\n\`close\`\nClose the ticket.\n\n\`open\`\nOpen the ticket.\n\n\`delete\`\nDelete the ticket.\n\n\`transcript\`\nSend the ticket transcript. `)
+      .setDescription(`This help page shows all the command of this bot.\n\n**Commands**\n\`help\`\nShow the help page.\n\n\`ping\`\nReturns bot ping. `)
       .setFooter(`Subscribe to Cody Dimensions YouTube Channel`)
       .setThumbnail(client.user.displayAvatarURL({ dynamic: true, size: 4096 }))
       message.channel.send({ embeds: [embed] });
